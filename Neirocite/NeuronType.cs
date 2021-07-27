d5cc7ace5fbfa28f25be7b841e23fea25755635a
Эльдар <@DESKTOP-OL19TCI>
2021-07-27 15:28:41 +0300

    Добавьте файлы проекта.

---
+++ Neirocite/NeuronType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neirocite
+{
+
+    public enum NeuronType
+    { 
+        Input = 0,
+        Normal = 1,
+        Output = 2
+    }
+
+
+}
